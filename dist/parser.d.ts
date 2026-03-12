dd3d19923dee1125224eebaacfbef70aa4b588bc
Dave Thomas <dave.thomas@devo.com>
2026-03-12 15:52:34 -0500

    archive

--- dist/parser.d.ts
+++
@@ -1,3 +0,0 @@
-import { type PList, type Pik } from './types.ts';
-export declare function pikParse(p: Pik, input: string): PList | null;
-//# sourceMappingURL=parser.d.ts.map
--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
         exports: 'named',
       },
     },
+    minify: 'esbuild',
     sourcemap: true,
   },
 });
